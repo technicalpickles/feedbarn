b179b1d07fae3d21cc1712e6181092063f5dc59a
Josh Nichols <josh@technicalpickles.com>
2008-02-22 13:00:01 -0500

    New 'barns' directory to put barn configs in. Farmer now loads all blueprints for barns at initialization time.

--- lib/feedbarn/farmer.rb
+++ lib/feedbarn/farmer.rb
@@ -8,28 +8,30 @@ module FeedBarn
   class Farmer
     CONFIG = 'config/feeds.yaml'
     
-    attr_accessor :config, :barns
+    attr_accessor :config, :barns, :blueprints
     
     def initialize()
       self.config = YAML.load_file('config/settings.yml')
       self.barns = {}
+      
+      self.blueprints = {}
+      Dir.glob('config/barns/*.yml').each do |config_path|
+        barn_name = yank_barn_name_from config_path
+        self.blueprints[barn_name] = load_blueprint_from config_path
+      end
     end
     
-    def feed_urls
-      self.config['feeds'].collect { |feed|
-        feed['url']
-      }
-    end
-    
+    # Builds out a barn
     def build barn_name
-      barn_config = load_config_for barn_name
-      barns[barn_config['title']] = Barn.new(barn_name, barn_config)
+      blueprint = blueprints[barn_name]
+      barns[blueprint['title']] = Barn.new(barn_name, blueprint)
     end
     
+    # Populates a barn with feeds
     def fill barn
       barn.feeds = []
 
-      load_config_for(barn.name)['feeds'].each do |feed_config|
+      blueprints[barn.name]['feeds'].each do |feed_config|
         url = feed_config['url']
         downloaded_feed = FeedNormalizer::FeedNormalizer.parse(open(url))
         barn.feeds << Feed.new(downloaded_feed, feed_config)
@@ -38,13 +40,20 @@ module FeedBarn
       barn.feeds
     end
     
+    # Gets a showcase ready
     def prepare_showcase barn
       FeedBarn::Showcase.new(barn)
     end
     
     private
-    def load_config_for barn_name
-      YAML.load_file("config/#{barn_name}.yml")
+    
+    # Loads a config a particular barn
+    def load_blueprint_from barn_yml
+      YAML.load_file(barn_yml)
+    end
+    
+    def yank_barn_name_from filename
+      File.basename(filename).gsub(/\.yml$/,'')
     end
   end
 end
